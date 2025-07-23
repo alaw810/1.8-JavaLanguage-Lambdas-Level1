1d88f2a50b471171a7028c92850b95cb3282bb80
Adrià Lorente <adria810@gmail.com>
2025-07-23 11:23:40 +0200

    feat(ex3): print all month names using lambda expression

--- src/main/java/com/alaw810/ex3/MainEx3.java
+++ src/main/java/com/alaw810/ex3/MainEx3.java
@@ -1,4 +1,12 @@
 package com.alaw810.ex3;
 
+import java.util.List;
+
 public class MainEx3 {
+    public static void main(String[] args) {
+        List<String> names = List.of("January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December");
+        names.forEach(name -> System.out.println(name));
+
+    }
 }
