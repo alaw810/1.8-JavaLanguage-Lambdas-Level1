dd87e7dc17320c445a994e2740fe51f7a5825de9
Adrià Lorente <adria810@gmail.com>
2025-07-23 12:35:41 +0200

    feat(ex7): sort string list by length from longest to shortest

--- src/main/java/com/alaw810/ex7/MainEx7.java
+++ src/main/java/com/alaw810/ex7/MainEx7.java
@@ -1,15 +1,15 @@
-package com.alaw810.ex6;
+package com.alaw810.ex7;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-public class MainEx6 {
+public class MainEx7 {
     public static void main(String[] args) {
         List<String> list = new ArrayList<>(List.of("123456789", "Java", "ITAcademy", "Hello", "999", "Lambdas"));
         System.out.println("List before sorting: " + list);
 
-        list.sort(Comparator.comparingInt(String::length));
+        list.sort(Comparator.comparingInt(String::length).reversed());
         System.out.println("List after sorting: " + list);
 
     }
