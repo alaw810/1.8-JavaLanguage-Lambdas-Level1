9a3d12efbea25ac375a27a5a265c5430d91e9989
Adrià Lorente <adria810@gmail.com>
2025-07-23 11:22:58 +0200

    feat(ex2): filter names containing 'o' and longer than 5 characters

--- src/main/java/com/alaw810/ex2/MainEx2.java
+++ src/main/java/com/alaw810/ex2/MainEx2.java
@@ -1,4 +1,16 @@
 package com.alaw810.ex2;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class MainEx2 {
+    public static void main(String[] args) {
+        List<String> names = List.of("Paul", "John", "Mark", "Monica", "Oscar", "Rachel");
+        List<String> namesWithO = names.stream()
+                .filter(name -> name.toLowerCase().contains("o") && name.length() > 5)
+                .collect(Collectors.toList());
+
+        System.out.println(namesWithO);
+
+    }
 }
