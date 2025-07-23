7a06e0b4c1bf18404d726146842540c64156c383
Adrià Lorente <adria810@gmail.com>
2025-07-23 11:21:31 +0200

    feat(ex1): filter names containing letter 'o'

--- src/main/java/com/alaw810/ex1/MainEx1.java
+++ src/main/java/com/alaw810/ex1/MainEx1.java
@@ -1,4 +1,16 @@
 package com.alaw810.ex1;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class MainEx1 {
+    public static void main(String[] args) {
+        List<String> names = List.of("Paul", "John", "Mark", "Monica", "Oscar", "Rachel");
+        List<String> namesWithO = names.stream()
+                .filter(name -> name.toLowerCase().contains("o"))
+                .collect(Collectors.toList());
+
+        System.out.println(namesWithO);
+
+    }
 }
